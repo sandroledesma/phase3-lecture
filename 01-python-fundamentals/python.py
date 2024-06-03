9d037d549a2a237ae1571d5fd12cfdf444d99330
Alessandro Ledesma <sandroledesma@gmail.com>
2024-06-03 15:32:24 -0400

    update files

--- 01-python-fundamentals/python.py
+++ 01-python-fundamentals/python.py
@@ -1,24 +1,37 @@
 # Variables
 my_var = 1  # we use snake case in Python
-MY_CONSTANT = "don't change me!"  # All variables are changeable in Python, programmers use all caps to signal when something should be a constant
+MY_CONSTANT = "don't change me!"  
+# All variables are changeable in Python, programmers 
+# use all caps to signal when something should be a constant
 
 # Functions
-def my_func(param1, param2):
+def my_func(param1, param2): # short for define
     pass # do something
 
+def my_function(x, y):
+    result = x + y
+    return result
+
+my_result = my_function(4, 5)
+print(my_result)
+
+# best practice is to define functions
 
 # Simple data types
-my_int = 0
-my_float = 3.14
-my_bool = True
-my_string = "hello"  # or 'hello' or '''hello'''
-my_list = [1, 2, 3, 4]
-my_dictionary = {'name': 'joe', 'age': 21}
-my_none = None
+my_int = 0 # integer (int)
+my_float = 3.14 # floating point number (float), 3.0 is also a float
+my_bool = True # boolean (bool) True or False
+my_string = "hello"  # or 'hello' or '''hello''' (str)
+my_list = [1, 2, 3, 4] # array/list (list)
+my_dictionary = { #@ dictionary (dict)
+    'name': 'joe', 
+    'age': 21
+}
+my_none = None # (NonType)
 
 
 # Type checking
-type(my_int)
+type(my_int) # will return <class 'int'>
 
 # Type conversion
 int('1')
@@ -67,17 +80,36 @@ True and True  # AND => True
 True or False  # OR => True
 not True       # NOT => False
 
+x = 1 
+y = 3
+if not y == 3 and x == 0 or y > 10: # PEMDAS
+    print('y is three and x is zero')
+else: 
+    print(':(')
+
 # while loop
 while False:
     pass  # loop code
 
+x = 10 
+while x < 10: 
+    print('x is less than 10')
+    x += 1 # the same as x = x + 1
+
+my_numbers = [1, 2, 3, 4, 5]
+for num in my_numbers:
+    print(num)
+
+for i in range (0, 5):
+    print(i, my_numbers[i])
 
 # for loop
 for i in range(10):
     pass  # loop code
 
 
+
 # for item in collection
 seq = ['a', 'b', 'c']
 for char in seq:
-    pass  # loop code
+    pass  # loop code``
